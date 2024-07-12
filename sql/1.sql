3fb18e95c11bf1dd2c3fc7705c3e92e94b0936af
Mahdi Haghighi <mahdi.haghighi94@gmail.com>
2024-07-12 17:37:25 +0330

    Update schima create db sql code to postgres

--- sql/1.sql
+++ sql/1.sql
@@ -1,63 +1,19 @@
--- Drop the function if it already exists
-DROP FUNCTION IF EXISTS uuid_generate_v4;
-
--- Create the UUID generation function
-CREATE FUNCTION uuid_generate_v4() RETURNS CHAR(36) 
-    DETERMINISTIC 
-    RETURN LOWER(CONCAT(
-        SUBSTRING(MD5(RAND()) FROM 1 FOR 8), '-',
-        SUBSTRING(MD5(RAND()) FROM 9 FOR 4), '-4',
-        SUBSTRING(MD5(RAND()) FROM 13 FOR 3), '-',
-        SUBSTRING('89ab', FLOOR(RAND()*4+1), 1),
-        SUBSTRING(MD5(RAND()) FROM 17 FOR 3), '-',
-        SUBSTRING(MD5(RAND()) FROM 21 FOR 12)
-    ));
-
--- Create the users table without the default value for id
-CREATE TABLE users (
-    id CHAR(36) PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password TEXT,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+CREATE extension if not exists citext;
+create extension if not exists "uuid-ossp";
+CREATE table users (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY key,
+    username citext UNIQUE not null,
+    password text,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
-
--- Create a trigger to set the default value for id
-DELIMITER //
-CREATE TRIGGER before_insert_users
-BEFORE INSERT ON users
-FOR EACH ROW
-BEGIN
-    IF NEW.id IS NULL THEN
-        SET NEW.id = uuid_generate_v4();
-    END IF;
-END;
-//
-DELIMITER ;
-
--- Create the notes table without the default value for id
-CREATE TABLE notes (
-    id CHAR(36) PRIMARY KEY,
-    parent_id CHAR(36),
-    user_id CHAR(36) NOT NULL,
-    title TEXT,
-    content TEXT,
-    is_published BOOLEAN NOT NULL DEFAULT FALSE,
-    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (parent_id) REFERENCES notes (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+CREATE table notes (
+    id uuid default uuid`_generate_v4() PRIMARY key,
+    parent_id uuid references notes (id),
+    user_id uuid not null references users (id),
+    title text,
+    content text,
+    is_published boolean not null default false,
+    cerated_at timestamp default now(),
+    updated_at timestamp default now()
 );
-
--- Create a trigger to set the default value for id
-DELIMITER //
-CREATE TRIGGER before_insert_notes
-BEFORE INSERT ON notes
-FOR EACH ROW
-BEGIN
-    IF NEW.id IS NULL THEN
-        SET NEW.id = uuid_generate_v4();
-    END IF;
-END;
-//
-DELIMITER ;
